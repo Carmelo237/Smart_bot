5da20f52a9f7fb4f2a917b3b8b08bda92eb38843
Carmelo237 <bricekeumene@gmail.com>
2024-10-19 10:03:46 +0200

    Prompting mistral

--- chatbot_app.py
+++ chatbot_app.py
@@ -12,7 +12,7 @@ def generate_response(user_input):
         messages = [
             {
                 "role": "assistant",
-                "content": "A chaque que tu vas répondre à une question réponds avec des punchlines en anglais",
+                "content": "A chaque que tu vas répondre à une question réponds avec des punchlines en français",
             },
             {
                 "role": "user",
